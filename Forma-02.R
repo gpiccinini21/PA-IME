3e4f95d4e412d06327dec74cb57c24dbddcd0b76
Gianfranco Piccinini <gianfranco.piccinini@usach.cl>
2022-01-27 19:03:40 -0300

    Importaciones

--- Forma-02.R
+++ Forma-02.R
@@ -1,1 +1,31 @@
 #Forma-02
+
+#Librerías
+if (!require(ggpubr) ) {
+  install.packages("ggpubr", dependencies = TRUE )
+  require (ggpubr)
+}
+if (!require(tidyverse) ) {
+  install.packages("tidyverse", dependencies = TRUE )
+  require (tidyverse)
+}
+if (!require(dplyr) ) {
+  install.packages("dplyr", dependencies = TRUE )
+  require (dplyr)
+}
+if (!require(ez) ) {
+  install.packages("ez", dependencies = TRUE )
+  require (ez)
+}
+if (!require(boot) ) {
+  install.packages("boot", dependencies = TRUE )
+  require (boot)
+}
+if (!require(simpleboot) ) {
+  install.packages("simpleboot", dependencies = TRUE )
+  require (simpleboot)
+}
+if (!require(bootES) ) {
+  install.packages("bootES", dependencies = TRUE )
+  require (bootES)
+}
